b9de328ef3378c4ee9794bfb18eb74644f5cdf7b
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-31 09:42:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// Imports code from Firebase.google.com so that the initalizeApp function can be used
+// Imports function from Firebase.google.com so that the initalizeApp function can be used
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js"
+
+// Imports database data from Firebase.google.com so that the initalizeApp function can be used
+import { getDatabase,
+         ref,
+         push
+ } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js"
 
   //web app's Firebase configuration
 const firebaseConfig = {
+    databaseURL: process.env.DATABASE_URL
 }
 
  // Initialize Firebase
  const app = initializeApp(firebaseConfig)
+
+ const database = getDatabase(app)
+ const referenceInDB = ref(database, "leads")
 
 
 
@@ -14,7 +24,7 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    push(referenceInDB, inputEl.value)
     
     console.log(inputValue)
 })
