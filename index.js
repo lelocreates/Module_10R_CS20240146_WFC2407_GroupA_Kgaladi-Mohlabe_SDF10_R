7ef47ed1ac35ce7dc808091f27f359c65517a693
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-07-31 10:22:35 +0200

    Update JS file to fix errors in my code

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ import { getDatabase,
 
   //web app's Firebase configuration
 const firebaseConfig = {
-    databaseURL: process.env.DATABASE_URL
+    databaseURL: "https://leads-tracker-app-38c17-default-rtdb.europe-west1.firebasedatabase.app/"
+
 }
 
  // Initialize Firebase
@@ -20,11 +21,40 @@ const firebaseConfig = {
 
 
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 
-addButtonEl.addEventListener("click", function() {
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
+onValue(referenceInDB, function(snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
+})
+
+inputBtn.addEventListener("click", function() {
     push(referenceInDB, inputEl.value)
-    
-    console.log(inputValue)
+    inputEl.value = "" 
 })
